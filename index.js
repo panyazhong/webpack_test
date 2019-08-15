900016280ed4c2e6548725b30423706af2c64b8b
panyazhong <pyz1226149198@gmail.com>
2019-08-15 17:24:37 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import Header from './src/Header'
+
+new Header()
+
---
+++ src/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+	const dom = document.getElementById('root')
+
+	const header = document.createElement('div')
+	header.innerText = 'header'
+	dom.appendChild(header)
+}
